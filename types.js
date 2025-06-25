d3d7eb7270882bf6115efe8d031ce485a62de97e
sohanur28 <gsrahman28@gmail.com>
2025-06-25 18:43:11 +0600

    Test commit

--- types.js
+++ types.js
@@ -18,7 +18,7 @@ const d = true;
 const ages = [45, 65, 48];
 
 const student = {id: 23, class: 7}
-console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
 
 let x = 5;
 let y = x;
