b5e58d444eb45a97cb9d421549989809844057a9
sohanur28 <sohanur28@gmail.com>
2025-06-25 17:28:41 +0600

    types

---
+++ types.js
@@ -0,0 +1,15 @@
+// strongly typed language
+// int a = 5;
+// String b = 'alim halim khalim';
+// bool c = true;
+// object student = {name: 'noya daman', id: 55}
+// int[] numbers = [12, 45, 78]
+// string[] friends = ['abul', 'babul'];
+
+// dynamic type Language
+const a = 5;
+const b = "Samsu kopai na ekhon ar";
+const ages = [45, 65, 48];
+const student = {id: 23, class: 7}
+console.log(typeof a, typeof b, typeof ages, typeof student);
+
