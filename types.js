94b785181f35d436eae8b4d2064bf286ef13db8b
sohanur28 <sohanur28@gmail.com>
2025-06-25 17:40:37 +0600

    primitive-non-primitive

--- types.js
+++ types.js
@@ -1,15 +1,32 @@
 // strongly typed language
 // int a = 5;
 // String b = 'alim halim khalim';
-// bool c = true;
+// boolean c = true;
 // object student = {name: 'noya daman', id: 55}
 // int[] numbers = [12, 45, 78]
 // string[] friends = ['abul', 'babul'];
 
-// dynamic type Language
+// JavaScript is a dynamic type Language
+
+// primitive type
 const a = 5;
 const b = "Samsu kopai na ekhon ar";
-const ages = [45, 65, 48];
-const student = {id: 23, class: 7}
-console.log(typeof a, typeof b, typeof ages, typeof student);
+const d = true;
 
+
+// non-primitive
+const ages = [45, 65, 48];
+
+const student = {id: 23, class: 7}
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+const p = {job: 'web developer'}
+let q = p;
+q.job = 'front end developer';
+console.log(p, q);
