04946a6a8c440c0937fef0ce7acd50fe33380dfe
sohanur28 <gsrahman28@gmail.com>
2025-06-29 12:56:53 +0600

    (==) vs (===)

---
+++ triple.js
@@ -0,0 +1,14 @@
+// always use ===
+// equal comparison doesn't work for non-primitive
+
+const first = [45, 54, 98];
+const seceond = [45, 54, 98];
+if (first === seceond) {
+  console.log("values are equal");
+} else {
+  console.log("values are not equal");
+}
+
+// type coercion
+// type conversion
+// type casting
